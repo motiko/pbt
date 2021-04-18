df5d008970982dff7094a34c14e76db76efed042
moti <moti.kor@gmail.com>
2021-04-18 22:32:07 +0200

    newGame

--- pages/game/new.js
+++ pages/game/new.js
@@ -1,21 +1,27 @@
 import { API } from "aws-amplify";
 import { useRouter } from "next/router";
-// import { createBattle } from "../../src/graphql/mutations";
+import { createBattle } from "../../src/graphql/mutations";
+import { v4 as uuid } from "uuid";
 
 function NewGame() {
   const router = useRouter();
   async function newGame() {
     console.log("new");
-    // const battle = await API.graphql({
-      // query: createBattle,
-    // });
-    // console.log(battle);
-    // router.push(`/game/${battle.id}`);
+    console.log(id);
+    const data = await API.graphql({
+      query: createBattle,
+      variables: {
+        input: { puzzleId: "533d5d38-80fe-4d7d-8e7a-2cb06af8277a" },
+      },
+    });
+    console.log(data);
+    const id = data.data.createBattle.id;
+    router.push(`/game/${id}`);
   }
   return (
     <form>
       <div className="flex flex-col">
-        <label for="difficulty">Difficulty</label>
+        <label htmlFor="difficulty">Difficulty</label>
         <select id="difficulty">
           <option value="easy">Easy</option>
           <option value="medium">Medium</option>
