afc29ecd608518dcc7b4042b8e89ae3461ac54d2
moti <moti.kor@gmail.com>
2021-04-25 14:57:29 +0200

    fixup! fixup! server side create and read

--- pages/game/[id].tsx
+++ pages/game/[id].tsx
@@ -8,14 +8,10 @@ const GamePage = (props) => {
 export default GamePage;
 
 export async function getServerSideProps(context) {
-  // console.log(context);
   const { query } = context;
-  console.log(query);
   const snapshot = await rtdb.ref(`games/${query.id}`).get();
-  console.log(snapshot);
   const game = await snapshot.val();
   const puzzle = game.currentPuzzle;
-  console.log(game);
   return {
     props: {
       initialFen: puzzle.fen,
